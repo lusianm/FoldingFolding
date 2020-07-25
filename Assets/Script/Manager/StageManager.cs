8399f62c2e22691f157ff8fe36b6c51aa5dd30bb
lusianm <47287650+lusianm@users.noreply.github.com>
2020-07-25 16:35:17 +0900

    Player Prefab Reload
    Player Prefab Reload

--- Assets/Script/Manager/StageManager.cs
+++ Assets/Script/Manager/StageManager.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour, ISceneManagerInterface
 {
+    MapManager mapManager;
+    GameManager gameManager;
+
+    //Stage 초기화 함수
     public void Init()
     {
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mapManager = MapManager.instance;
+        gameManager = GameManager.instance;
+    }
+    public void SceneRestart()
+    {
+        gameManager.SceneRestart();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
