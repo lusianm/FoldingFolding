86e9a9e9a398f2bf09674a519554611d169c75a1
lusianm <47287650+lusianm@users.noreply.github.com>
2020-07-26 09:50:44 +0900

    Saw Folding Edit
    Saw Folding Edit

--- Assets/Script/Manager/ChapterSelectionManager.cs
+++ Assets/Script/Manager/ChapterSelectionManager.cs
@@ -19,7 +19,7 @@ public class ChapterSelectionManager : MonoBehaviour, ISceneManagerInterface
 
     IEnumerator SceneChangeTime(string sceneName)
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         GameManager.instance.SceneChange(sceneName);
     }
 }
